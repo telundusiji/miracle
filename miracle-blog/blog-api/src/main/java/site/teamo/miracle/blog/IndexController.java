b3a3d32a50fa9cd7e0e5f770ef58b357bd0c7a2e
haocongshun <haocongshun@hikvision.com.cn>
2018-12-18 19:05:22 +0800

    初始化

---
+++ miracle-blog/blog-api/src/main/java/site/teamo/miracle/blog/IndexController.java
@@ -0,0 +1,8 @@
+package site.teamo.miracle.blog;
+
+/**
+ * @author haocongshun
+ * create on 2018/12/18 19:02
+ */
+public class IndexController {
+}
---
+++ miracle-blog/blog-start/src/main/java/site/teamo/miracle/blog/BlogApplication.java
@@ -0,0 +1,15 @@
+package site.teamo.miracle.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haocongshun
+ * create on 2018/12/18 18:58
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}

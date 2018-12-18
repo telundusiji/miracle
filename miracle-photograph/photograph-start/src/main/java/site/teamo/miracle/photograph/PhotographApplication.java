bdd6217980c8eb9df12455b1aaa2208dc929a6c9
haocongshun <haocongshun@hikvision.com.cn>
2018-12-18 18:10:50 +0800

    首次提交初始化

---
+++ miracle-photograph/photograph-start/src/main/java/site/teamo/miracle/photograph/PhotographApplication.java
@@ -0,0 +1,15 @@
+package site.teamo.miracle.photograph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haocongshun
+ * create on 2018/12/18 17:06
+ */
+@SpringBootApplication
+public class PhotographApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhotographApplication.class,args);
+    }
+}
